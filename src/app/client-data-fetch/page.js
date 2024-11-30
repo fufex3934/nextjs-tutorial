48d93d732239a7abe908ad29c59f6c1c6857d38d
fufex3934 <fufawakgari174@gmail.com>
2024-11-30 15:59:54 +0300

    client side data fetching

--- src/app/client-data-fetch/page.js
+++ src/app/client-data-fetch/page.js
@@ -1,9 +1,50 @@
-'use client'
+"use client";
+
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
 
 const ClientDataFetch = () => {
-  return (
-    <div>ClientDataFetch</div>
-  )
-}
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default ClientDataFetch
+  const fetchListOfUsers = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch("https://dummyjson.com/users");
+      const data = await response.json();
+      setUsers(data.users);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchListOfUsers();
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading users...</p>
+      ) : (
+        <ul>
+          {users.length > 0 ? (
+            users.map((user) => (
+              <li key={user.id} className="mt-5 cursor-pointer">
+                <Link href={`/server-data-fetch/${user.id}`}>
+                  {user.firstName}
+                </Link>
+              </li>
+            ))
+          ) : (
+            <p>No users found.</p>
+          )}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default ClientDataFetch;
