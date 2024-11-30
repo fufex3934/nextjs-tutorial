b4ec2a0ecd20c81bd28da9cf867fcfe0644bf4f4
fufex3934 <fufawakgari174@gmail.com>
2024-11-30 14:25:45 +0300

    redirect using server component

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -1,9 +1,13 @@
+import { redirect } from 'next/navigation';
 import React from 'react'
 
 const Account = () => {
-  return (
-    <div>Account</div>
-  )
+    const userProfile = null;
+  if (userProfile === null) redirect('profile')
+    return   <div>
+  <h1>Account Page</h1>
+
+</div>
 }
 
 export default Account
